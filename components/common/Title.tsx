2d8045ff4be42cd94332a43b219c3ea3aa3859cd
shriharip <shrihari.p4@gmail.com>
2023-02-15 22:11:14 +0100

    added "Beta" on title

--- components/common/Title.tsx
+++ components/common/Title.tsx
@@ -4,7 +4,7 @@ export default function Title({ title }: { title: string }) {
   return (
     <div>
       <Head>
-				<title>{title} | Overlapp</title>
+				<title>{title} | Overlapp <span className='text-sm' >Beta</span></title>
       </Head>
     </div>
   );
--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -2,7 +2,7 @@ export default function HomeBanner() {
   return (
 		<div className='text-black shadow-gray-300'>
       <div className='container flex flex-col items-center mx-auto py-8 space-y-2'>
-				<h1 className='font-titillium font-extrabold text-6xl drop-shadow-lg'>Overlapp</h1>
+				<h1 className='font-titillium font-extrabold text-6xl drop-shadow-lg'>Overlapp <span className="text-sm"> Beta</span></h1>
 				<p className='font-light text-xl sm:text-2xl whitespace-nowrap'>A place to share your interests.</p>
       </div>
     </div>
